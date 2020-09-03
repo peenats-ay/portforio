27b1b88565a08620863d66c62de71159cb167855
peenats-ay <65844340+peenats-ay@users.noreply.github.com>
2020-09-03 19:42:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 'use strict';
 
 $(function(){
+    $('header a').click(function(){
+        var id = $(this).attr('href');
+        var position = $(id).offset().top;
+        $('html, body').animate({
+            'scrollTop' : position
+        }, 500);
+    });
+
+    $('.index-btn').click(function() {
+        $('.active').removeClass('active');
+        var clickedIndex = $('.index-btn').index($(this));
+        $('.song').eq(clickedIndex).addClass('active');
+      });    
+
     $('.contact-icon').hover(function(){
         $(this).animate({
             'font-size' : '20px'
@@ -13,11 +27,4 @@ $(function(){
     }
     );
 
-    $('header a').click(function(){
-        var id = $(this).attr('href');
-        var position = $(id).offset().top;
-        $('html, body').animate({
-            'scrollTop' : position
-        }, 500);
-    });
 });
