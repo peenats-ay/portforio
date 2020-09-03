0aff7f9da1e05a9d9bbf4b4d6618613e327c9a0e
peenats-ay <65844340+peenats-ay@users.noreply.github.com>
2020-09-03 18:49:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 'use strict';
 
-{
-    document.getElementById('playSong').play;
-    document.getElementById('playSong').pause;
+$(function(){
+    $('.contact-icon').hover(function(){
+        $(this).animate({
+            'font-size' : '20px'
+        }, 300);
+    },
+    function(){
+        $(this).animate({
+            'font-size' : '17px'
+        }, 300);
+    }
+    );
 
-    
-}
+    $('header a').click(function(){
+        var id = $(this).attr('href');
+        var position = $(id).offset().top;
+        $('html, body').animate({
+            'scrollTop' : position
+        }, 500);
+    });
+});
